10ff0e518dc1908f2dd4489678ce5e9516f669ef
GadHao <524472212@qq.com>
2022-04-16 22:50:47 +0800

    feat: minimum-depth-of-binary-tree

---
+++ solutions/python_src/111.二叉树的最小深度.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode.cn id=111 lang=python3
+#
+# [111] 二叉树的最小深度
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDepth(self, root: TreeNode) -> int:
+        if root is None:
+            return 0
+        if root.left is None:
+            return self.minDepth(root.right) + 1
+        if root.right is None:
+            return self.minDepth(root.left) + 1
+        return min(self.minDepth(root.left) + 1, self.minDepth(root.right) + 1)
+# @lc code=end
